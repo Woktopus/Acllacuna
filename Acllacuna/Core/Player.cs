1f921bb26967c41e43f8d6027a252d563356820e
TomTomCantNot <cantenot.thomas@wanadoo.fr>
2016-01-29 22:44:02 +0100

    Added player

---
+++ Acllacuna/Core/Player.cs
@@ -0,0 +1,32 @@
+﻿using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Acllacuna
+{
+	class Player
+	{
+		Body body;
+
+		public Player()
+		{
+		}
+
+		public void LoadContent(World world, Vector2 position)
+		{
+			Vector2 size = new Vector2(0.5f, 1.5f);
+
+			body = BodyFactory.CreateRectangle(world, size.X, size.Y, 1f);
+			body.BodyType = BodyType.Dynamic;
+			body.Position = position;
+
+			CircleShape circle = new CircleShape(size.X / 2, 1f);
+
+			circle.Position = new Vector2(0, size.Y / 2);
+
+			body.CreateFixture(circle);
+		}
+	}
+}
