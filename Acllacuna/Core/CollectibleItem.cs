f189c6efb5897c5cf04cf1af30b63a9709467a8e
Auguste Meyer <augustemeyer86@gmail.com>
2016-01-30 12:07:31 +0100

    Add Enemy & collectibleItem

---
+++ Acllacuna/Core/CollectibleItem.cs
@@ -0,0 +1,22 @@
+﻿using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Acllacuna
+{
+    public class CollectibleItem
+    {
+        public Body body { get; set; }
+        public Image image { get; set; }
+
+        public CollectibleItem()
+        {
+            image = new Image();
+
+        }
+
+    }
+}
--- Acllacuna/Core/Enemy.cs
+++ Acllacuna/Core/Enemy.cs
@@ -1,4 +1,7 @@
-﻿using FarseerPhysics.Dynamics;
+﻿using FarseerPhysics;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using System;
@@ -23,9 +26,28 @@ namespace Acllacuna
             contactsWithFloor = 0;
         }
 
-        public void LoadContent(World world, ContentManager content, Vector2 size, Vector2 position, string texturePath)
+        public void LoadContent(World world, ContentManager content, Vector2 size, Vector2 position, string texturePath, Color textureColor)
         {
+            body = BodyFactory.CreateRectangle(world, size.X, size.Y - (size.X / 2), 1f);
+            body.BodyType = BodyType.Dynamic;
+            body.FixedRotation = true;
+            body.Position = position;
+            body.Mass = 50;
+
+            CircleShape circle = new CircleShape(size.X / 2, 1f);
+            circle.Position = new Vector2(0, (size.Y - (size.X / 2)) / 2);
+
+            Fixture feet = body.CreateFixture(circle);
+            feet.UserData = (int)4;
+
+            image.LoadContent(content, texturePath, textureColor, GetDrawPosition());
+
             this.size = size;
+        }
+
+        private Vector2 GetDrawPosition()
+        {
+            return ConvertUnits.ToDisplayUnits(body.Position + new Vector2(0, (size.Y / 2) / 2));
         }
 
     }
