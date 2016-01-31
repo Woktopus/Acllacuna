c51a26fbad209b92bd1d19b487645b2a9d5149e5
Briland Mathieu <briland.mathieu@gmail.com>
2016-01-31 12:31:11 +0100

    tile

--- Acllacuna/Core/Map.cs
+++ Acllacuna/Core/Map.cs
@@ -36,11 +36,16 @@ namespace Acllacuna
             listSpike = new List<Spike>();
             mondico = new Dictionary<int, string>();
             mondico.Add(1, "Graphics/TileMap/cactus_bottom");
-            mondico.Add(2, "Graphics/TileMap/cube2");
+            mondico.Add(2, "Graphics/TileMap/stoneSol");
             mondico.Add(3, "Graphics/TileMap/spike");
             mondico.Add(4, "Graphics/TileMap/spike_bas");
             mondico.Add(5, "Graphics/TileMap/spike_droite");
             mondico.Add(6, "Graphics/TileMap/spike_gauche");
+            mondico.Add(7, "Graphics/TileMap/cactus_top");
+            mondico.Add(8, "Graphics/TileMap/torchStone");
+            mondico.Add(9, "Graphics/TileMap/stonebrick");
+            mondico.Add(10, "Graphics/TileMap/furnace");
+            mondico.Add(11, "Graphics/TileMap/furnace2");
             pathMap = path;
             pars = new Parser("Map/Map1.txt");
             
