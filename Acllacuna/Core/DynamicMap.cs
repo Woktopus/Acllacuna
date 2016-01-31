cd6bc7b3c780ad41189fee143abbb313142b5624
Auguste Meyer <augustemeyer86@gmail.com>
2016-01-31 12:58:46 +0100

    edit map1

--- Acllacuna/Core/DynamicMap.cs
+++ Acllacuna/Core/DynamicMap.cs
@@ -24,8 +24,8 @@ namespace Acllacuna
         {
             this.path = path;
             mondico = new Dictionary<int, string>();
-            mondico.Add(1, "Graphics/TileMap/stonebrick");
-            mondico.Add(2, "Graphics/TileMap/stonebrick");
+            mondico.Add(1, "Graphics/TileMap/cactus_bottom");
+            mondico.Add(2, "Graphics/TileMap/cactus_bottom");
             parse = new Parser("Map/Map1Dyn.txt");
             listMov = new List<MovingPlatforme>();
             listEnnemy = new List<Enemy>();
